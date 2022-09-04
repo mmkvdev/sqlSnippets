3cd4b7e3f3bbb754f933b1a0d2d2a799926232a8
mmkvdev <madhuv299@gmail.com>
2022-09-04 20:28:27 +0530

    concept/select: select query examples

---
+++ statements/select/init.sql
@@ -0,0 +1,11 @@
+-- command to get all the columns from a table
+SELECT * FROM actor;
+
+-- command to get only specific column from a table
+SELECT first_name FROM actor;
+
+-- command to get multiple columns from a table
+SELECT first_name, last_name FROM actor;
+
+-- command to get multiple columns from a table (order doesn't matter)
+SELECT last_name, first_name FROM actor;
