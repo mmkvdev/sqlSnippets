d16f2be261025e9d0db866e9895d99af831d2a52
Madhu Kiran Varma <28980183+mmkvdev@users.noreply.github.com>
2022-09-24 20:52:21 +0530

    sql/between: adding conditions for BETWEEN across different dates

--- statements/select/between/init.sql
+++ statements/select/between/init.sql
@@ -22,8 +22,26 @@ WHERE amount NOT BETWEEN 8 AND 9;
 
 -- get all the payments that happened between the first half of february
 SELECT * FROM payment
-WHERE payment_date BETWEEN '2007-02-01' AND '2007-02-15';
+WHERE payment_date BETWEEN '2007-02-01' AND '2007-02-16'
+ORDER BY payment_date DESC;
 
 -- COUNT
 SELECT COUNT(*) FROM payment
-WHERE payment_date BETWEEN '2007-02-01' AND '2007-02-16';
+WHERE payment_date BETWEEN '2007-02-01' AND '2007-02-15';
+
+
+SELECT * FROM payment
+WHERE payment_date >= '2007-02-01' OR payment_date <= '2007-02-14';
+
+
+
+SELECT DISTINCT(payment_date) 
+FROM payment 
+WHERE payment_date >= '2007-02-01' AND payment_date <= '2007-02-28' 
+ORDER BY payment_date;
+
+
+SELECT DISTINCT(payment_date) 
+FROM payment 
+WHERE payment_date = '2007-02-15' 
+ORDER BY payment_date DESC;
